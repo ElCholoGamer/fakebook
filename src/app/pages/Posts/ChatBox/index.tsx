28d1a495f47fffd683a22b862f5c859309a0678b
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-02 13:53:29 -0500

    responsive chatbox

--- src/app/pages/Posts/ChatBox/index.tsx
+++ src/app/pages/Posts/ChatBox/index.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import { XCircleFill } from 'react-bootstrap-icons';
 import Form from 'react-bootstrap/Form';
 import './ChatBox.css';
 
-const ChatBox: React.FC = () => {
+interface Props {
+	setChat: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ChatBox: React.FC<Props> = ({ setChat }) => {
 	return (
 		<div
 			id="chat-wrapper"
 			className="d-flex flex-column border-left border-secondary">
-			<h3 className="bg-primary p-3 text-light m-0">Live Chat</h3>
+			<h3 className="bg-primary p-3 text-light m-0">
+				<XCircleFill
+					id="chat-close"
+					onClick={() => setChat(false)}
+					className="mr-2"
+				/>
+				Live Chat
+			</h3>
 			<div id="chat-box">
+				{/* Placeholder text */}
 				{[
 					[
-						'JoeMama',
-						'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+						'PersonOnTheInternet',
+						'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nostrum, repellendus ',
 					],
 					[
 						'JoeMama',
-						'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quae, ex facilis sed est',
+						'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quae, ex facilis sed est, corrupti neque aspernatur quaerat dolor illo dolorum.',
 					],
-					['Ligma', 'Lorem ipsum dolor sit amet'],
+					['PersonOnTheInternet', 'Bruh'],
 				].map(i => (
-					<div key="" className="chat-message border-bottom p-2">
+					<div
+						key={JSON.stringify(i)}
+						className="chat-message border-bottom p-2">
 						<h6 className="m-1">{i[0]}:</h6>
 						<p className="m-1">{i[1]}</p>
 					</div>
--- src/app/pages/Posts/index.tsx
+++ src/app/pages/Posts/index.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/esm/Button';
 import ChatBox from './ChatBox';
 import './Posts.css';
 
 const Posts: React.FC = () => {
-	return (
-		<div id="posts-page-wrapper" className="d-flex flex-row">
-			<div id="posts-wrapper" className="p-3">
-				<h1>Latest posts</h1>
-				<hr />
+	const { clientWidth } = document.body;
+	const [chat, setChat] = useState(clientWidth > 800);
 
-				<div id="posts-content" style={{ height: 1000 }}>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
-					inventore laborum nam distinctio quae nobis nulla asperiores maxime
-					suscipit pariatur, voluptatem, iusto quam, dolor alias accusamus
-					temporibus! Assumenda, nam maxime.
+	return (
+		<div
+			id="posts-page-wrapper"
+			className="d-flex flex-row justify-content-stretch">
+			{(clientWidth > 500 || !chat) && (
+				<div id="posts-wrapper" className="p-3">
+					<h1>Latest posts</h1>
+					<hr />
+
+					<div id="posts-content" style={{ height: 1000 }}>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
+						inventore laborum nam distinctio quae nobis nulla asperiores maxime
+						suscipit pariatur, voluptatem, iusto quam, dolor alias accusamus
+						temporibus! Assumenda, nam maxime.
+					</div>
 				</div>
-			</div>
-			<ChatBox />
+			)}
+			{chat ? (
+				<ChatBox setChat={setChat} />
+			) : (
+				<Button
+					id="chat-button"
+					className="m-5"
+					onClick={() => setChat(true)}
+					variant="primary">
+					Open Chat
+				</Button>
+			)}
 		</div>
 	);
 };
