8915569b41b5198b1d18e7842e6cac628a45b6e0
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-04 12:46:43 -0500

    add markdown to messages

--- src/app/pages/Posts/ChatBox/Message.tsx
+++ src/app/pages/Posts/ChatBox/Message.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { ChatMessage } from '../../../utils';
+import { ChatMessage, User } from '../../../utils';
+import { Remarkable } from 'remarkable';
 
 interface Props {
 	data: ChatMessage;
+	user: User | null;
 }
 
-const Message: React.FC<Props> = ({ data: { content, author } }) => (
+const md = new Remarkable();
+const Message: React.FC<Props> = ({ data: { content, author }, user }) => (
 	<div className="chat-message border-bottom p-2">
 		<h6 className="m-1">
 			<a className="text-dark" href={`/account/${author._id}`}>
-				{author.username}:
+				{author._id === user?._id ? 'You' : user.username}:
 			</a>
 		</h6>
-		<p className="m-1">{content}</p>
+		<p
+			className="m-1"
+			dangerouslySetInnerHTML={{ __html: md.render(content) }}></p>
 	</div>
 );
 
