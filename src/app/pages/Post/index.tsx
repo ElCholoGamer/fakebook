0943623df7208e1d28136baebe6c9e3da9e0eef4
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-08 12:23:26 -0500

    add author and like counter to post

--- src/app/pages/Post/index.tsx
+++ src/app/pages/Post/index.tsx
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import React, { useEffect, useState } from 'react';
+import { HeartFill, Heart } from 'react-bootstrap-icons';
 import { useHistory, useParams } from 'react-router-dom';
 import Loading from '../../components/Loading';
 import { Post as IPost, User } from '../../utils';
@@ -9,10 +10,11 @@ interface Props {
 	user: User | null;
 }
 
-const Post: React.FC<Props> = () => {
+const Post: React.FC<Props> = ({ user }) => {
 	const { id } = useParams<{ id: string }>();
 	const history = useHistory();
 
+	const [liking, setLiking] = useState(false);
 	const [post, setPost] = useState<IPost | null>(null);
 
 	useEffect(() => {
@@ -22,10 +24,10 @@ const Post: React.FC<Props> = () => {
 			axios.get(`/api/posts/${id}`).then(res => setPost(res.data.post));
 
 		fetchPost().catch((err: AxiosError) => {
-			console.error(err);
 			if (err.response?.status === 404) {
 				history.push('/posts');
 			} else if (mounted) {
+				console.error(err);
 				setTimeout(fetchPost, 3000);
 			}
 		});
@@ -37,15 +39,61 @@ const Post: React.FC<Props> = () => {
 
 	if (!post) return <Loading />;
 
-	const { title, image, content } = post;
+	const handleClick = () => {
+		if (!post || liking) return;
+		if (!user) return alert('Log in to like posts (bruh)');
+
+		setLiking(true);
+		axios
+			.put(`/api/posts/${id}/like`)
+			.then(res => setPost(res.data.post))
+			.catch(console.error)
+			.finally(() => setLiking(false));
+	};
+
+	const { title, image, content, likes, author, createdAt } = post;
+	const d = new Date(createdAt);
+
+	const iconProps = {
+		className: `mr-2${liking || !user ? '' : ' free'}`,
+		onClick: handleClick,
+	};
+
 	return (
 		<div className="post-wrapper">
 			<div className="post p-5 mx-auto my-5 rounded">
-				<h4 className="mb-4">{title}</h4>
+				<h4 className="mb-2">{title}</h4>
 				{content && <p>{content}</p>}
+
 				{image && (
-					<img className="post-image rounded" src={`/api/posts/${id}/image`} />
+					<img
+						className="post-image rounded mt-2"
+						src={`/api/posts/${id}/image`}
+					/>
 				)}
+
+				<div className="my-3">
+					<img
+						className="rounded-circle mr-2 post-avatar"
+						src={`/api/avatar/${author._id}`}
+					/>
+					<span>
+						By{' '}
+						<a className="text-dark" href={`/user/${author._id}`}>
+							{author.username}
+						</a>
+						{'  '}| {d.getDate()}-{d.getMonth() + 1}-{d.getFullYear()}
+					</span>
+				</div>
+
+				<div className="post-likes my-2">
+					{likes.includes(user?._id || '') ? (
+						<HeartFill {...iconProps} />
+					) : (
+						<Heart {...iconProps} />
+					)}
+					{likes.length}
+				</div>
 			</div>
 		</div>
 	);
